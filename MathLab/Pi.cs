00c87b5baab03b5869e2b3f1c00dc736e0a2f79a
PC\eyerj <joel.eyer@bwz-rappi.ch>
2025-08-04 17:19:32 +0200

    add rnd Pi and not finshed Intergral Pi

---
+++ MathLab/Pi.cs
@@ -0,0 +1,31 @@
+﻿namespace MathLab;
+
+public class Pi
+{
+    public static double CalculateRnd(long rounds)
+    {
+        Random rnd = new Random();
+
+        long inCircle = 0;
+        
+        for (long i = 0; i < rounds; i++)
+        {
+            double rndX = rnd.NextDouble();
+            double rndY = rnd.NextDouble();
+            inCircle = (rndX * rndX + rndY * rndY <= 1) ? inCircle + 1 : inCircle;
+        }
+        
+        return (double)inCircle * 4 / rounds;
+    }
+
+    public static double CalculateIntegral(int rectangels)
+    {
+        double area = 0;
+        double xStep = (double)1 / rectangels;
+        for (double x = xStep; x <= 1D; x+=xStep)
+        {
+            area += xStep * Math.Sqrt(4 - x * x);
+        }
+        return area;
+    }
+}
---
+++ MathLab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Pi.CalculateRnd(10000000));
+            Console.WriteLine(Pi.CalculateIntegral(100));
+        }
+    }
+}
